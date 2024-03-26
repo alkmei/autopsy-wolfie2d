db8005be81d0521a74cda88fde7e9124f7085b73
Alvin Mei <alvinkmei@gmail.com>
2024-03-25 20:28:01 -0400

    gulp build work?

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,50 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var watchify = require('watchify');
-var tsify = require('tsify');
-var fancy_log = require('fancy-log');
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var watchify = require("watchify");
+var tsify = require("tsify");
+var fancy_log = require("fancy-log");
+const { exit } = require("process");
 var paths = {
-    pages: ['src/*.html']
+  pages: ["src/*.html"],
 };
 
-var watchedBrowserify = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-}).plugin(tsify));
-
-gulp.task('copy-html', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+var b = browserify({
+  basedir: ".",
+  debug: true,
+  entries: ["src/main.ts"],
+  cache: {},
+  packageCache: {},
 });
 
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .on('error', fancy_log)
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist'));
+b.plugin(tsify);
+
+var watchedBrowserify = watchify(b);
+
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+
+function prodBundle() {
+  return b.bundle().pipe(source("bundle.js")).pipe(gulp.dest("dist"));
 }
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', fancy_log);
+function bundle() {
+  return watchedBrowserify
+    .bundle()
+    .on("error", fancy_log)
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
+}
+
+gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+gulp.task(
+  "build",
+  gulp.series(gulp.parallel("copy-html"), prodBundle, done => {
+    done();
+    exit(0);
+  }),
+);
+
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", fancy_log);
