6f68f4b311808803feadbd14ee902c587a97eee1
Yuxiang <yu-xiang.zheng@stonybrook.edu>
2024-04-21 20:16:14 -0400

    change lvl 3 to lvl 4

--- src/Autopsy/Scenes/Levels/Level4.ts
+++ src/Autopsy/Scenes/Levels/Level4.ts
@@ -1,11 +1,15 @@
 import GameLevel from "../GameLevel";
 import Vec2 from "../../../Wolfie2D/DataTypes/Vec2";
-import Level5 from "./Level5";
+import Level5 from "./Level3";
+import WaveLevel from "../WaveLevel/WaveLevel";
+import Wave from "../WaveLevel/Wave";
+import Ghost from "@/Autopsy/Enemy/Ghost/Ghost";
+import OrthogonalTilemap from "@/Wolfie2D/Nodes/Tilemaps/OrthogonalTilemap";
 
-export default class Level4 extends GameLevel {
+export default class Level3 extends WaveLevel {
   loadScene() {
     super.loadScene();
-    this.load.tilemap("tilemap", "assets/tilemaps/Debug/Level1.json");
+    this.load.tilemap("tilemap", "assets/tilemaps/WaveTest(Level4)/Level4.json");
   }
 
   startScene() {
@@ -16,5 +20,18 @@ export default class Level4 extends GameLevel {
     this.viewport.setBounds(0, 0, 6400, 1280);
 
     this.nextLevel = Level5;
+    
+    this.setLevelEndArea(new Vec2(2877, 670), new Vec2(32, 128));
+
+    //this.waves=[new Wave([1])];
+    let tmap = this.getTilemap('World') as OrthogonalTilemap;
+    this.setTilemap = tmap;
+    
+
+  
+    this.waves=[new Wave([3]),new Wave([9]),new Wave([27])]
+
+    this.startWave();
+
   }
 }
