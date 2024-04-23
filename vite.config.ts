d57d4fe9b0779432ee160e38a39fd14c1c18ee26
Alvin Mei <alvinkmei@gmail.com>
2024-04-22 22:32:56 -0400

    added benchmarks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        benchmark1: resolve(__dirname, "benchmark1/index.html"),
+        benchmark2: resolve(__dirname, "benchmark2/index.html"),
+      },
+    },
+  },
 });
